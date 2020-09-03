e43f87d2a4e739c7fc82e09166bf9d01eba48d37
dream-k <topdev.star@gmail.com>
2020-09-03 21:19:37 +0800

    customtext1

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,6 @@
     <b-container fluid>
       <router-view/>
     </b-container>
-<<<<<<< HEAD
-    <CustomText1 />
-    <CustomText2 />
-    <Manufacturer />
-=======
->>>>>>> 51a514a69c256869c6f128b90910d3526186773a
     <Contact />
     <Footer />
   </div>
@@ -21,12 +15,6 @@
 import HeaderNav from '@/components/HeaderNav'
 import HeaderBefore from '@/components/HeaderBefore'
 import HeaderTop from '@/components/HeaderTop'
-<<<<<<< HEAD
-import CustomText1 from '@/components/CustomText1'
-import CustomText2 from '@/components/CustomText2'
-import Manufacturer from '@/components/Manufacturer'
-=======
->>>>>>> 51a514a69c256869c6f128b90910d3526186773a
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 export default {
@@ -35,12 +23,6 @@ export default {
     HeaderNav,
     HeaderBefore,
     HeaderTop,
-<<<<<<< HEAD
-    CustomText1,
-    CustomText2,
-    Manufacturer,
-=======
->>>>>>> 51a514a69c256869c6f128b90910d3526186773a
     Contact,
     Footer
   }
