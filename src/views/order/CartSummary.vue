d8b1c632e32dcf7bd4d2b7e6b623757ddc3bfaa0
Star Coder <starcoder204@gmail.com>
2020-09-09 17:33:55 +0200

    update order page

---
+++ src/views/order/CartSummary.vue
@@ -0,0 +1,223 @@
+<template>
+    <div class="cart-summary">
+        <div class="header">
+            <label>{{$t('order.overview')}}</label>
+        </div>
+        <div class="right-address-block">
+            <div id="delivery-addresses" class="address-selector">
+                <article class="address-item id-address-delivery-address-class  selected">
+                    <header>
+                        <label class="radio-block">
+                            <span class="address-alias">
+                                <b-icon icon="house-door"></b-icon>
+                                Shipping Address
+                            </span>
+                        </label>
+                        <a href="" class="edit-address">
+                            <b-icon icon="pencil"></b-icon>
+                        </a>
+                    </header>
+                    <div class="address">
+                        <span> Qiang Dong </span>
+                        <br>
+                        <br>
+                        <span> Jin er street </span>
+                        <br>
+                        <span> 23000 </span> <span> Dalian city </span>
+                        <br>
+                        China
+                    </div>
+                </article>
+            </div>
+            <div id="invoice-addresses" class="address-selector">
+                <article class="address-item">
+                    <header>
+                        <label class="radio-block">
+                            <span class="address-alias">
+                                <b-icon icon="receipt"></b-icon>
+                                Invoice Address
+                            </span>
+                        </label>
+                        <a href="" class="edit-address">
+                            <b-icon icon="pencil"></b-icon>
+                        </a>
+                    </header>
+                    <div class="address">
+                        <span> Qiang Dong </span>
+                        <br>
+                        <br>
+                        <span> Jin er street </span>
+                        <br>
+                        <span> 23000 </span> <span> Dalian city </span>
+                        <br>
+                        China
+                    </div>
+                </article>
+            </div>
+        </div>
+        <section class="js-cart cart_right_summary_data">
+            <div class="card-block card-block-cart-summery">
+                <div class="cart-summary-products">
+                </div>
+                <div class="cart-summary-line cart-summary-subtotals">
+                    <span class="label">
+                    Total Net:
+                    </span>
+                    <span class="value">
+                    1.539,68 €
+                    </span>
+                </div>
+                <div class="cart-summary-line cart-summary-subtotals">
+                    <span class="label">
+                    + 19% MwSt.
+                    </span>
+                    <span class="value">
+                    292.54€
+                    </span>
+                </div>
+                <div class="cart-summary-line cart-summary-subtotals">
+                    <span class="label">
+                    {{$t('order.shipping')}}
+                    </span>
+                    <span class="value">
+                    {{$t('order.free')}}
+                    </span>
+                </div>
+            </div>
+            <div class="hidden">
+                <div class="cart-voucher">
+                    <b-input-group>
+                        <b-form-input
+                            :type="'text'"
+                            :placeholder="$t('order.coupon_code')"
+                        ></b-form-input>
+                        <b-input-group-append>
+                            <b-button variant="primary">{{$t('order.use')}}</b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                </div>
+            </div>
+            <div class="card-block cart-summary-totals">
+                <div class="cart-summary-line cart-total">
+                    <span class="label">
+                        {{$t('order.total')}}
+                    </span>
+                    <span class="value">
+                        1.832,22 €
+                    </span>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+.cart-summary {
+    background: #12407e;
+    background: linear-gradient(90deg, rgba(18,64,126,1) 0%, rgba(17,61,119,1) 68%, rgba(14,49,96,1) 100%);
+    -webkit-transition: all .45s cubic-bezier(.3,0,.15,1);
+    transition: all .45s cubic-bezier(.3,0,.15,1);
+    min-height: 400px;
+    margin-bottom: 20px;
+    border: 1px solid #12407e;
+    box-shadow: 0px 3px 6px #00000029;
+    padding: 30px 0px;
+    border-radius: 8px;
+    color: #fff;
+    .header {
+        color: #f0f0f0;
+        font-size: 16px;
+        text-transform: uppercase;
+        text-align: center;
+        border-bottom: 1px solid #E9E9E9;
+        margin: 0px 1.5rem;
+        padding-bottom: 10px;
+        margin-bottom: 20px;
+    }
+    .right-address-block {
+        text-align: left;
+        .address-selector {
+            padding: 0px 1.5rem;
+            .address-item {
+                margin-bottom: 10px !important;
+                border-bottom: .5px solid #f0f0f0;
+                padding-bottom: 15px;
+                .radio-block {
+                    color: #232323;
+                    font-size: .875rem;
+                    display: inline-block;
+                    text-align: right;
+                    .address-alias {
+                        color: #fff;
+                        font-weight: 500;
+                        font-size: 16px;
+                        svg {
+                            font-weight: 500;
+                            font-size: 16px;
+                            color: #fff;
+                            margin-right: 10px;
+                        }
+                    }
+                }
+                .edit-address {
+                    float: right;
+                    color: #fff;
+                }
+                .address {
+                    font-size: 14px;
+                    font-weight: 300;
+                }
+            }
+        }
+    }
+    .cart-summary-line {
+        clear: both;
+        margin-bottom: .315rem;
+        span {
+            font-size: 16px;
+            font-weight: 300;
+        }
+        .label {
+            padding-left: 0 !important;
+        }
+        .value {
+            float: right !important;
+        }
+    }
+    .cart-voucher {
+        margin-bottom: 20px;
+        margin-top: 30px;
+        .btn-primary {
+            background-color: #ccc;
+            color: #113b72;
+            font-weight: 500;
+            border: none;
+            min-width: 100px;
+        }
+        .btn-primary:hover {
+            background-color: #b2162c;
+            color: #fff;
+        }
+    }
+    .cart-summary-totals {
+        padding: 1rem 2rem 0 !important;
+        border-top: #F0F0F0 1px solid;
+        text-align: left;
+        .cart-total {
+            background: transparent;
+        }
+    }
+}
+.cart_right_summary_data {
+    padding: 0px 25px 0px;
+}
+.card-block-cart-summery {
+    text-align: left;
+    padding: 1rem 2rem 0 !important;
+}
+</style>
--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -1,11 +1,172 @@
 <template>
-  <div class="order m-5">
-    <h1> This is order page </h1>
+  <div class="order mt-5">
+    <section>
+      <div class="page-navheader text-center">
+          <div class="payment-detail-btn">
+            <b-row class="lastorder-section-row">
+              <b-col cols="4" class="sp1">
+                <b-col md="12">
+                  <b-row>
+                    <div id="per-address-checkout" class="step check-step step1">
+                      <b-icon icon="check2"></b-icon>
+                    </div>
+                  </b-row>
+                </b-col>
+                <div class="desc">
+                  <span>{{$t('order.delivery_and_billing_address')}}</span>
+                </div>
+              </b-col>
+              <b-col cols="4" class="sp2">
+                <b-col md="12">
+                  <b-row>
+                    <div id="shipping-checkout" class="step step2">
+                      <b-icon icon="check2"></b-icon>
+                    </div>
+                  </b-row>
+                </b-col>
+                <div class="desc">
+                  <span>{{$t('order.shipping_and_payment_method')}}</span>
+                </div>
+              </b-col>
+              <b-col cols="4" class="sp4">
+                <b-col md="12">
+                  <b-row>
+                    <div id="confirem-checkout" class="step step4">
+                      <b-icon icon="check2"></b-icon>
+                    </div>
+                  </b-row>
+                </b-col>
+                <div class="desc">
+                  <span>{{$t('order.confirmation')}}</span>
+                </div>
+              </b-col>
+            </b-row>
+          </div>
+      </div>
+    </section>
+    <div class="customer-info">
+      <b-container>
+        <b-row>
+          <b-col md="8">
+            <Step1 />
+          </b-col>
+          <b-col md="4" class="mart-70 cart-right-mart">
+            <CartSummary />
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
 <script>
+import Step1 from './Step1'
+import CartSummary from './CartSummary'
 export default {
-  name: 'order'
+  name: 'order',
+  components: {
+    Step1,
+    CartSummary
+  }
 }
 </script>
+
+<style scoped lang="scss">
+.order {
+  section {
+    margin-bottom: 1.563rem;
+    .page-navheader {
+      margin: 50px auto 100px;
+      color: #12407D;
+      font-weight: 700;
+      .payment-detail-btn {
+        margin: 0 auto;
+        padding: 0;
+        position: relative;
+        width: 60%;
+        .lastorder-section-row {
+          margin-right: -140px;
+          margin-left: -140px;
+          .step {
+            height: 70px;
+            width: 70px;
+            background: #ccc;
+            border-radius: 50%;
+            float: none;
+            margin: auto;
+            text-align: center;
+            line-height: 80px;
+            color: #fff;
+            svg {
+              font-size: 26px;
+            }
+          }
+          .step2::before, .step4::before {
+            content: '';
+            background-color: #ccc;
+            height: 25px;
+            display: block;
+            position: absolute;
+            top: 22px;
+            left: -50%;
+            width: 95%;
+          }
+          .check-step {
+            color: #fff;
+            background: #12407E;
+            font-size: 24px;
+            line-height: 70px;
+            z-index: 4;
+          }
+          .desc {
+            span {
+              position: relative;
+              top: 15px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+@media (min-width: 1200px) {
+  .row {
+    margin-right: -15px !important;
+    margin-left: -15px !important;
+  }
+}
+@media screen and (max-width: 768px) {
+  .payment-detail-btn {
+    width: 100% !important;
+  }
+  .lastorder-section-row {
+    margin-right: -15px !important;
+    margin-left: -15px !important;
+    padding-top: 15px !important;
+    .sp1, .sp2 {
+      padding-right: 0px !important;
+    }
+  }
+  .step {
+    width: 40px !important;
+    height: 40px !important;
+    line-height: 40px !important;
+    svg {
+      width: 24px !important;
+      height: 24px !important;
+      line-height: 20px !important;
+      margin-left: 2px !important;
+    }
+  }
+  .payment-detail-btn .desc span {
+    font-size: 12px !important;
+    top: 5px !important;
+    position: relative !important;
+  }
+  .step2::before, .step4::before {
+    top: 10px !important;
+    height: 15px !important;
+    left: -55% !important;
+  }
+}
+</style>
