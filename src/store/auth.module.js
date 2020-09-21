f21a39fdebc9ba78b2561c8a3d1f853f79e54439
Star Coder <starcoder204@gmail.com>
2020-09-21 17:49:50 +0200

    update user auth state management

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -1,7 +1,7 @@
 const state = {
   status: '',
-  token: localStorage.getItem('token') || '',
-  user: {}
+  token: localStorage.getItem('ekugellager_token') || '',
+  user: JSON.parse(localStorage.getItem('user')) || null
 }
 
 const mutations = {
@@ -13,6 +13,8 @@ const mutations = {
     state.status = 'success'
     state.token = data.token
     state.user = data.user
+    localStorage.setItem('ekugellager_token', data.token)
+    localStorage.setItem('ekugellager_user', JSON.stringify(data.user))
   },
   auth_error (state) {
     state.status = 'error'
@@ -20,7 +22,9 @@ const mutations = {
   logout (state) {
     state.status = ''
     state.token = ''
-    state.user = {}
+    state.user = null
+    localStorage.removeItem('ekugellager_token')
+    localStorage.removeItem('ekugellager_user')
   }
 }
 
@@ -29,45 +33,16 @@ const actions = {
     return new Promise((resolve, reject) => {
       commit('auth_request')
       commit('auth_success', { token: 'token', user: payload }) // for TEST
-    // axios({url: 'http://localhost:3000/login', data: user, method: 'POST' })
-    // .then(resp => {
-    //   const token = resp.data.token
-    //   const user = resp.data.user
-    //   localStorage.setItem('token', token)
-    //   axios.defaults.headers.common['Authorization'] = token
-    //   commit('auth_success', token, user)
-    //   resolve(resp)
-    // })
-    // .catch(err => {
-    //   commit('auth_error')
-    //   localStorage.removeItem('token')
-    //   reject(err)
-    // })
     })
   },
   register ({ commit }, user) {
     return new Promise((resolve, reject) => {
       commit('auth_request')
-    // axios({url: 'http://localhost:3000/register', data: user, method: 'POST' })
-    // .then(resp => {
-    //   const token = resp.data.token
-    //   const user = resp.data.user
-    //   localStorage.setItem('token', token)
-    //   axios.defaults.headers.common['Authorization'] = token
-    //   commit('auth_success', token, user)
-    //   resolve(resp)
-    // })
-    // .catch(err => {
-    //   commit('auth_error', err)
-    //   localStorage.removeItem('token')
-    //   reject(err)
-    // })
     })
   },
   logout ({ commit }) {
     return new Promise((resolve, reject) => {
       commit('logout')
-      localStorage.removeItem('token')
       resolve()
     })
   }
@@ -75,7 +50,10 @@ const actions = {
 
 const getters = {
   isLoggedIn: state => !!state.token,
-  authStatus: state => state.status
+  authStatus: state => state.status,
+  user (state) {
+    return state.user
+  }
 }
 
 export default {
