e454795a24ebef9f3882b03d90e87618110f8518
Star Coder <starcoder204@gmail.com>
2020-11-03 16:56:32 +0100

    register modal, forgot modal

--- src/constants/modal.js
+++ src/constants/modal.js
@@ -1,3 +1,5 @@
 export const SHOPPING_CART_MODAL = 'shopping_cart_modal'
 export const BLOCK_CART_MODAL = 'blockcart_modal'
 export const LOGIN_MODAL = 'login_modal'
+export const REGISTER_MODAL = 'register_modal'
+export const FORGOT_MODAL = 'forgot_modal'
